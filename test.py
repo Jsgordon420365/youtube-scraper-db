c9087dc8994c669aa525efabcc638f5bff20e955
Jeffy Gordon <jsgordon420@gmail.com>
2025-04-16 13:15:48 -0400

    Update .gitignore and remove tracked files that should be ignored

--- test.py
+++
@@ -1,8 +0,0 @@
-from pytube import YouTube
-
-url = "https://www.youtube.com/watch?v=Tx0Y3qb9ZUc"
-try:
-    yt = YouTube(url)
-    print("Title:", yt.title)
-except Exception as e:
-    print("Error fetching video:", e)
